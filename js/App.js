3e79b9634df78e570dbc7d5cd537050e025cf826
Lenlil <55378996+Lenlil@users.noreply.github.com>
2020-03-16 12:21:08 +0100

    Snyggat till lite

--- js/App.js
+++ js/App.js
@@ -1,25 +1,23 @@
 
 var converter = new Converter();
-
-var lastFetchedRates = new Date().getHours() - 2;
-var timeSinceCheck;
-var sessionRatesObject;
-
 var sellCurrency = document.querySelector("#sellCurrency");
 var buyCurrency = document.querySelector("#buyCurrency");
 
-GetApiData();
+// Cache nedan
+var lastFetchedRates = new Date().getHours() - 2;
+var timeSinceCheck;
+
+FetchApiData();
 DisplayData();
 
 var convertButton = document.querySelector("#convert");
-
 convertButton.addEventListener('click', () => {
 
     let fromCurrency = sellCurrency.value;
     let toCurrency = buyCurrency.value; 
     let amount = amountToConvert.value;
     
-    GetApiData();
+    FetchApiData();
     let result = converter.Exchange(fromCurrency, toCurrency, amount);
 
     let displayResult = document.querySelector("#amountPostConversion"); 
@@ -29,7 +27,7 @@ convertButton.addEventListener('click', () => {
 });
 
 
-async function GetApiData()
+async function FetchApiData()
 {    
     var url = 'https://api.exchangeratesapi.io/latest?base=SEK';
     
@@ -43,18 +41,13 @@ async function GetApiData()
                         
         sessionStorage.setItem("rates", JSON.stringify(currencyRates));
 
-        lastFetchedRates = new Date().getHours();              
-     
-    }   
-    else
-    {
-        sessionRatesObject = JSON.parse(sessionStorage.getItem("rates"));              
-    }    
+        lastFetchedRates = new Date().getHours();                  
+    }       
 }
 
 function DisplayData()
 {
-    sessionRatesObject = JSON.parse(sessionStorage.getItem("rates"));
+    let sessionRatesObject = JSON.parse(sessionStorage.getItem("rates"));
 
     for(var rate in sessionRatesObject.rates)
     {
